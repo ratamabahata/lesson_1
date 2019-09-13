57c1356e23744f460f3a90ade4bcb3bade1820c9
$ ruby -v <shirma-michail@mail.ru>
2019-09-13 17:11:58 +0300

    3 commit

--- IdealWeight.rb
+++ IdealWeight.rb
@@ -2,9 +2,9 @@ puts "What's your name?"
 name = gets.chomp
 puts "What's your height?"
 height = gets.chomp.to_i
-ideal.weight = height - 110
+ideal_weight = height - 110
 if ideal_weight < 0
   puts "Ваш вес уже оптимальный"
 else
-  puts "#{name}, ваш идеальный вес - #{perfect_weight}"
+  puts "#{name}, ваш идеальный вес - #{ideal_weight}"
 end
--- TrueTriangle.rb
+++ TrueTriangle.rb
@@ -9,19 +9,16 @@ puts "Введите третью сторону треугольника:"
 triangle_3 = gets.chomp.to_i
 
 # Сортировка сторон по длине
-hypot, 1_katet, 2_katet = [triangle_1, triangle_2, triangle_3].sort.reverse
+katet_1, katet_2, hypot = [triangle_1, triangle_2, triangle_3].sort
 
-#Проверяем теориму Пифагора на прямоугольный
-if hypot**2 == 1_katet**2 + 2_katet**2
-  puts "Треугольник прямоугольный"
-end
-
-#Проверяем на равностороний
-if hypot == 1_katet and hypotenuse == 2_katet
-  puts "Треугольник равносторонний"
-end
-
-#Проверяем на равнобедренный
-if hypot == 1_katet or hypot == 2_katet or 1_katet = 2_katet
-  puts "Треугольник равнобедренный"
+if hypot**2 == katet_1**2 + katet_2**2
+  puts "Треугольник является прямоугольным"
+elsif hypot == katet_1 && hypot == katet_2
+  puts "Треугольник является равносторонним"
+elsif hypot == katet_1 ||
+      hypot == katet_2 ||
+      katet_1 == katet_2
+  puts "Треугольник является равнобедренным"
+else
+  puts "Треугольник не является ни прямоугольным, ни равнобедренным, ни равносторонним"
 end
